15f1cba2bd346ba77bbb8c0176cf9655ba785446
sergenorhann <sergenorhann@users.noreply.github.com>
2021-03-07 02:59:30 +0300

    Add project files.

---
+++ OOP2/Indıvidual.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class Indıvidual : Customer
+    {
+        public string TcNo { get; set; }
+        public string Name { get; set;  }
+        public string LastName { get; set; }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Indıvidual indıvidual1 = new Indıvidual();
+            indıvidual1.Id = 1;
+            indıvidual1.CustomerNumber = "125";
+            indıvidual1.Name = "Ali";
+            indıvidual1.LastName = "Yılmaz";
+            indıvidual1.TcNo = "1234567890";
+
+            Corporate corporate1 = new Corporate();
+            corporate1.Id = 2;
+            corporate1.CustomerNumber = "125";
+            corporate1.CompanyName = "Kodlama.io";
+            corporate1.TaxNumber = "234";
+
+            Customer indıvidual2 = new Indıvidual();
+            Customer corporate2 = new Corporate(); 
+
+
+
+
+        }
+    }
+}
